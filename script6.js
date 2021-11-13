414305f0006207bef7f81a70fdc6f11a0415f936
Alifudin Niko Priambodo <m.rozaki09903@gmail.com>
2021-11-13 19:22:00 +0700

    6.2 Destructuring Function

--- script6.js
+++ script6.js
@@ -72,15 +72,62 @@
 
 // mengambil field pada object, setelah dikirim sebgai
 // parameter untuk function
-const mhs = {
-    id: 22,
-    nama: 'Alifudinniko',
-    umur: 33
-}
+// const mhs = {
+//     id: 22,
+//     nama: 'Alifudinniko',
+//     umur: 33
+// }
 
-function getIdMhs({ id, nama }) {
-    return nama;
-}
+// function getIdMhs({ id, nama }) {
+//     return nama;
+// }
 
-console.log(getIdMhs(mhs));
+// console.log(getIdMhs(mhs));
+
+
+//4.2
+
+// Destructuring return value
+
+// function kalkulasi(a, b) {
+//     return {
+//         tambah: a + b,
+//         kurang: a - b,
+//         kali: a * b,
+//         bagi: a / b
+//     };
+// }
+// // const [jumlah, kali, bagi = 'tidak ada'] = kalkulasi(2, 3);
+// const { tambah, kurang, bagi, kali } = kalkulasi(6, 3);
+// console.log(bagi);
+
+//Destructuring function arguments
+
+//biasa
+const mhs1 = {
+    nama: 'alif',
+    nim: 'm3118007',
+    umur: 12,
+    nilai: {
+        tugas: 80,
+        uts: 88,
+        uas: 90,
+    }
+}
+// function cetak(mhs) {
+//     return `hai ${mhs.nama}, umurmu ${mhs.umur} kann`;
+// }
+// console.log(cetak(mhs1));
+
+//destructuring
+function cetak({ nama, umur, nilai: { tugas, uts, uas } }) {
+    return `hai ${nama}, umurmu ${umur} kann
+    selamat kamu mendapat nilai tugas ${uas}
+    `;
+}
+console.log(cetak(mhs1));
+
+
+
+
 
