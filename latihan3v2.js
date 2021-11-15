6ca8d81e64eb84f100283375cdb9ec7e329173e4
Alifudin Niko Priambodo <m.rozaki09903@gmail.com>
2021-11-15 12:36:02 +0700

    12. Fetch

---
+++ latihan3v2.js
@@ -0,0 +1,116 @@
+// $('.search-button').on('click', function () {
+
+//     $.ajax({
+//         url: 'http://www.omdbapi.com/?i=tt3896198&apikey=3a8df36d&s=' + $('.input-keywoard').val(),
+//         success: result => {
+//             console.log(result);
+
+//             const movies = result.Search;
+//             // console.log(movies);
+
+//             let cards = '';
+//             movies.forEach(m => {
+//                 cards += showCards(m);
+//             });
+//             //Jquery tolong ccarikan saya element movie lalu timpa pakai data cards
+//             $('.movie-container').html(cards);
+
+
+//             //Ketika Tombol detail di klik 
+//             $('.modal-detail-button').on('click', function () {
+//                 // console.log($(this).data('imdbid'));
+
+//                 $.ajax({
+//                     url: 'http://www.omdbapi.com/?apikey=3a8df36d&i=' + $(this).data('imdbid'),
+//                     success: m => {
+//                         const moviDetail = showCardsid(m);
+//                         $('.modal-body').html(moviDetail);
+//                     },
+//                     error: (e) => {
+//                         console.log(e.responseText);
+//                     }
+//                 });
+//             });
+//         },
+//         error: (e) => {
+//             console.log(e.responseText);
+//         }
+//     });
+
+// });
+
+// Fetch - - - - VANILA JAVASCRIPT
+
+const searchButton = document.querySelector('.search-button');
+searchButton.addEventListener('click', function () {
+
+    const inputKeyword = document.querySelector('.input-keyword');
+    fetch('http://www.omdbapi.com/?i=tt3896198&apikey=3a8df36d&s=' + inputKeyword.value)
+        .then(response => response.json())
+        .then(response => {
+            const movies = response.Search;
+            let cards = '';
+            movies.forEach(m => cards += showCards(m));
+            const movieContainer = document.querySelector('.movie-container');
+            movieContainer.innerHTML = cards;
+        });
+
+
+    //ketika tombol detail di-klik
+    const modalDetailButton = document.querySelectorAll('.modal-detail-button');
+    modalDetailButton.forEach(btn => {
+        btn.addEventListener('click', function () {
+            const imdbid = this.dataset.imdbid;
+            console.log(imdbid);
+            // fetch('http://www.omdbapi.com/?apikey=3a8df36d&i=' + imdbid)
+            //     .then(response => response.json())
+            //     .then(m => {
+            //         const moviDetail = showCardsid(m);
+            //         console.log(moviDetail);
+            //         const modalBody = document.querySelector('.modal-body');
+            //         modalBody.innerHTML = moviDetail;
+
+            //     });
+        });
+    });
+
+
+});
+
+function showCards(m) {
+    return `
+    <div class="col-md-4 my-3">
+    <div class="card">
+        <img src="${m.Poster}" class="card-img-top">
+        <div class="card-body">
+            <h5 class="card-title">${m.Title}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>
+            <a href="#" class="btn btn-primary modal-detail-button" data-bs-toggle="modal" data-bs-target="#moviemodal" data-imdbid="${m.imdbID}">Show Detail</a>
+        </div>
+    </div>
+    </div>
+    `;
+}
+
+function showCardsid(m) {
+    return `
+    <div class="conntainer-fluid">
+        <div class="row">
+            <div class="col-md-3">
+                <img src="${m.Poster}" alt="" class="">
+            </div>
+            <div class="col-md">
+                <ul class="list-group">
+                    <li class="list-group-item">
+                        <h4>${m.Title}</h4>
+                    </li>
+                    <li class="list-group-item"><strong>Direktor </strong>: ${m.Director}  </li>
+                    <li class="list-group-item"><strong>Aktor :</strong> ${m.Actors}</li>
+                    <li class="list-group-item"><strong>Writer :</strong> ${m.Writer}</li>
+                    <li class="list-group-item"><strong>Plot : </strong> ${m.Plot}</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    `;
+}
